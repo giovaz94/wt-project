6ca185f1d3a0df9d1d294999910e3c77c9b6e8bc
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2022-05-31 23:06:48 +0200

    Create order status migration

---
+++ migrations/m220531_210358_add_oder_status.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220531_210358_add_oder_status
+ */
+class m220531_210358_add_oder_status extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m220531_210358_add_oder_status cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220531_210358_add_oder_status cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
