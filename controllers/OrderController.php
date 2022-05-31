471709a96c1af5d98afcbaffaa6a5ab954f234a3
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2022-05-31 18:05:28 +0200

    Add Order Controller

---
+++ controllers/OrderController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Order;
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+
+class OrderController extends Controller
+{
+
+
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'access' => [
+                    'class' => AccessControl::class,
+                    'rules' => [
+                        [
+                            'allow' => true,
+                            'roles' => ['@']
+                        ],
+                    ],
+                ],
+            ]
+        );
+    }
+
+    public function actionIndex()
+    {
+        // Implemento la logica index del sito
+
+        // return $this->render("index", [
+        //  'searchModel' => $searchModel,
+        //  'dataProvider' => $dataProvider,
+        // ]);
+    }
+
+
+    public function actionDetail($idOrder)
+    {
+        // Implemento la logica del dettaglio del prodotto
+        $model = $this->findModel($idOrder);
+
+        // return $this->render("detail", [
+        //  'model' => $model
+        //]);
+    }
+
+
+    /**
+     * Finds the logged user Order model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param int $idOrder Id Order
+     * @return Order the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($idOrder)
+    {
+        if (($model = Order::findOne(['idOrder' => $idOrder, 'refUser' => Yii::$app->user->id])) !== null) {
+            return $model;
+        }
+        throw new NotFoundHttpException('The requested page does not exist.');
+
+    }
+}
