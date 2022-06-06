86f10710268f001a92e56548f805d275b9a688ec
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2022-06-06 14:17:45 +0200

    Add deliver role migration

---
+++ migrations/m220606_121028_add_deliver_role.php
@@ -0,0 +1,54 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220606_121028_add_deliver_role
+ */
+class m220606_121028_add_deliver_role extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $auth = Yii::$app->authManager;
+
+        /**
+         * ========================
+         * BEGIN: DELIVER PERMISSIONS
+         * ========================
+         */
+
+        $deliver= $auth->createRole("deliver");
+        $auth->add($deliver);
+
+        /**
+         * ========================
+         * END: DELIVER PERMISSIONS
+         * ========================
+         */
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        /**
+         * ========================
+         * BEGIN: DELIVER RIMOTION
+         * ========================
+         */
+
+        $auth = Yii::$app->authManager;
+        $auth->remove($auth->getRole("deliver"));
+
+        /**
+         * ========================
+         * END: DELIVER RIMOTION
+         * ========================
+         */
+    }
+}
